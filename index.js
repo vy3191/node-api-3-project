87151aaa6e4a460bc59353c294fe84c7eb271dc1
vy3191 <venkyafhs@gmail.com>
2020-02-18 20:16:17 -0500

    Added userRouter to the index.js

--- index.js
+++ index.js
@@ -2,29 +2,31 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRouter = require("./users/userRouter");
 const server = express();
 const PORT = 8500;
-
 
 server.use(helmet());
 server.use(morgan('tiny'));
 server.use(express.json());
+server.use("/api/users", userRouter);
+
 
 server.get("/api",(req,res) => {
    res.status(200).json({msg:'App is up and running now.'});
-})
-
+});
 server.use((req,res) => {
    res.status(404).json({
       message:'No Route Found'
    })
 });
-
 server.use((err,req,res,next) => {
     res.status(500).json({
        message: 'Something went wrong with the server'
     })
 });
+
+
 
 server.listen(PORT, (req,res) => {
     console.log(`Server is up and running at http://localhost:${PORT}`);
