45dd2ee673955e23af7e4cf876b22d520eed1f1e
vy3191 <venkyafhs@gmail.com>
2020-02-18 20:23:16 -0500

    completed-ValidateuserId function

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const userDb = require('./userDb');
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -32,8 +32,14 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-  // do your magic!
+function validateUserId(req, res, next) {  
+      const {id} = req.params;
+      userDb.getById(id)
+            .then( user => {
+               if(!user) res.status(404).json({ message: "invalid user id" });
+               req.user = user;
+            })
+            .catch(err => next(err));  
 }
 
 function validateUser(req, res, next) {
