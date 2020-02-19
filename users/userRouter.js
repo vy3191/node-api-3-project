d8f92bbcd8adc6592568bb07207ca66fccc7c5c0
vy3191 <venkyafhs@gmail.com>
2020-02-18 20:35:10 -0500

    Completed-get users

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,32 +2,64 @@ const express = require('express');
 const userDb = require('./userDb');
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', (req, res,next) => {
+    try {
+
+    }catch(err) {
+      next(err);
+    }
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', (req, res,next) => {
+    try {
+
+    }catch(err) {
+      next(err);
+    }
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', (req, res,next) => {
+    try {
+      userDb.get()
+            .then(users => {
+              if(!users) res.status(404).json({msg:'Users not found'});
+              res.json(users);
+            })
+    }catch(err) {
+      next(err);
+    }
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', (req, res,next) => {
+    try {
+
+    }catch(err) {
+      next(err);
+    }
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', (req, res,next) => {
+    try {
+
+    }catch(err) {
+      next(err);
+    }
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', (req, res,next) => {
+    try {
+
+    }catch(err) {
+      next(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', (req, res,next) => {
+    try {
+
+    }catch(err) {
+      next(err);
+    }
 });
 
 //custom middleware
@@ -54,7 +86,7 @@ function validateUser(req, res, next) {
 function validatePost(req, res, next) {
     if(!req.body) res.status(400).json({ message: "missing post data" });
     if(!req.body.text)  res.status(400).json({ message: "missing required text field" });
-    req.text = text;
+    req.post = text;
     next();
 } 
 
